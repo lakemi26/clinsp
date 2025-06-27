8048db8a87dcc67cdc60f456a3cf185de31cc111
lakemi26 <lakemi@outlook.com.br>
2025-06-27 00:29:01 -0300

    Fix: remove font não usada

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Raleway } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const ralewaySans = Raleway({
-  variable:"--font-raleway-sans",
-  weight: ['400', '500', '600', '700'],
-  subsets:["latin"],
+  variable: "--font-raleway-sans",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -31,10 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={ralewaySans.variable}>
-      <body
-        className={`${ralewaySans.variable} font-sans antialiased`}
-      >
-        <Navbar/>
+      <body className={`${ralewaySans.variable} font-sans antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
